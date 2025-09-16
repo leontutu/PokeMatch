9af7c4d18f6dd39fd83fa29e35430e5c60e6b6cf
leontutu <leo.vin.tut@gmail.com>
2025-09-16 11:54:25 +0200

    refactor: main js to ts

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { SocketProvider } from "./contexts/SocketContext.jsx";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <SocketProvider>
             <App />
