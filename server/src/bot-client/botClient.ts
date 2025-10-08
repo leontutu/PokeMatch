9dc0378f8cf4e54a409816e14f99b13451ee34b6
leontutu <leo.vin.tut@gmail.com>
2025-10-08 11:37:11 +0200

    feat: Add Botclient name enter and ready toggle

--- server/src/bot-client/botClient.ts
+++ server/src/bot-client/botClient.ts
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
+import { Events } from "../../../shared/constants/constants.js";
+import { delay } from "../utils/utils.js";
 
-export default function startBotClient(roomId: number) {
+export default async function startBotClient(roomId: number) {
     console.log("[BOTCLIENT] Started bot client with room ID:", roomId);
 
     const socket = io("http://localhost:3001", {
@@ -10,8 +12,14 @@ export default function startBotClient(roomId: number) {
     });
 
     socket.on("connect_error", (err: any) => {
-        console.error("Connection failed:", err.message);
+        console.error(`[BOTCLIENT-${roomId}] Connection failed:`, err.message);
     });
+
+    if (!socket) return;
+
+    socket.emit(Events.NAME_ENTER, "MrRobot");
+    await delay(500);
+    socket.emit(Events.TOGGLE_READY);
 }
 
 // const socket = import.meta.env.DEV
